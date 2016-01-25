7eb6b6a1fb46590877a13124f2da12d97abbd601
Romain FRANCOIS <rfrancois@CADICB.LOCAL>
2016-01-25 16:49:27 +0100

    Valeur par défaut dans une select

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var app = angular.module("app", []);
 
 app.controller("Controleur", ["$scope", "$filter", function ($scope, $filter) {
-	// le code du contrôleur
+	
+	// Le code du contrôleur
 	$scope.salles=[100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110];
 	$scope.enseignants=[{'loginEnseignant':'gchagnon', 'prenomEnseignant':'Gilles','nomEnseignant':'Chagnon'}, {'loginEnseignant':'pnollet', 'prenomEnseignant':'Patrick','nomEnseignant':'Nollet'}, {'loginEnseignant':'fhollande', 'prenomEnseignant':'François','nomEnseignant':'Hollande'}, {'loginEnseignant':'mlisa', 'prenomEnseignant':'Mona','nomEnseignant':'Lisa'}];
 	$scope.classe=[{'nomClasse':'Licence professionnelle Projet Web','effectifClasse': 25}, {'nomClasse':'Licence professionnelle ','effectifClasse': 30}, {'nomClasse':'Licence Physique','effectifClasse': 28}];
 	$scope.cours=[{'idEnseignant':0, 'idClasse':0, 'idSalle':100, 'date':'2016-01-23', 'heureDebut':8, 'heureFin':9}];
 
+	// Mettre le select de salle à une valeur par défaut
+	$scope.demande = {'classe':$scope.classe[0].nomClasse};
+	
 	$scope.ajout = function () {
 		console.log($filter('getEnseignantId')($scope.enseignants, $scope.demande.enseignant));
 		$scope.cours.push({});
