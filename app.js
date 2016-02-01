0725553bd34f40c07a0efcb2dfbabd7d0dc14462
Romain FRANCOIS <rfrancois@CADICB.LOCAL>
2016-02-01 16:36:14 +0100

    Date

--- app.js
+++ app.js
@@ -6,14 +6,39 @@ app.controller("Controleur", ["$scope", "$filter", function ($scope, $filter) {
 	$scope.salles=[100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110];
 	$scope.enseignants=[{'loginEnseignant':'gchagnon', 'prenomEnseignant':'Gilles','nomEnseignant':'Chagnon'}, {'loginEnseignant':'pnollet', 'prenomEnseignant':'Patrick','nomEnseignant':'Nollet'}, {'loginEnseignant':'fhollande', 'prenomEnseignant':'François','nomEnseignant':'Hollande'}, {'loginEnseignant':'mlisa', 'prenomEnseignant':'Mona','nomEnseignant':'Lisa'}];
 	$scope.classe=[{'nomClasse':'Licence professionnelle Projet Web','effectifClasse': 25}, {'nomClasse':'Licence professionnelle ','effectifClasse': 30}, {'nomClasse':'Licence Physique','effectifClasse': 28}];
-	$scope.cours=[{'idEnseignant':0, 'idClasse':0, 'idSalle':100, 'date':'2016-01-23', 'heureDebut':8, 'heureFin':9}];
+	$scope.cours=[{'idEnseignant':0, 'idClasse':0, 'idSalle':100, 'heureDebut':8, 'heureFin':9, 'videoProjecteur':1}];
 
 	// Mettre le select de salle à une valeur par défaut
-	$scope.demande = {'classe':$scope.classe[0].nomClasse};
+	//$scope.demande = {'classe':$scope.classe[0].nomClasse};
 	
 	$scope.ajout = function () {
 		console.log($filter('getEnseignantId')($scope.enseignants, $scope.demande.enseignant));
-		$scope.cours.push({});
+		console.log($scope.demande.classe);
+		
+		var idEnseignant = $filter('getEnseignantId')($scope.enseignants, $scope.demande.enseignant);
+		
+		// Si il manque des champs, arrêter la fonction ici
+		if(idEnseignant <= -1 || !$scope.demande.date || !$scope.demande.heureDebut || !$scope.demande.heureFin) {
+			return;
+		}
+		
+		// Formater la date dans les champs heureDebut et heureFin
+		$scope.demande.heureDebut.setDate($scope.demande.date.getDate());
+		$scope.demande.heureDebut.setMonth($scope.demande.date.getMonth());
+		$scope.demande.heureDebut.setFullYear($scope.demande.date.getFullYear());
+		$scope.demande.heureFin.setDate($scope.demande.date.getDate());
+		$scope.demande.heureFin.setMonth($scope.demande.date.getMonth());
+		$scope.demande.heureFin.setFullYear($scope.demande.date.getFullYear());
+		
+		// Envoyer les données dans le tableau
+		$scope.cours.push({
+			'idEnseignant': idEnseignant,
+			'idClasse':$scope.demande.classe,
+			'idSalle':$scope.demande.salle,
+			'heureDebut':$scope.demande.heureDebut,
+			'heureFin':$scope.demande.heureFin,
+			'videoProjecteur':$scope.demande.videoProjecteur,
+		});
 		//$scope.livres.push({'datePublication':$scope.chpanneepubli, 'nomAuteur':$scope.chpnom, 'prenomAuteur':$scope.chpprenom, 'titre':$scope.chptitre});
 	}
 }]);
